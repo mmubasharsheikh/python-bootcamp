4e2bfc8e0989c713a57aec037edef4d1674cb794
Mubashar Jamshad <m.mubasharsheikh@gmail.com>
2025-06-19 04:55:33 +0500

    First excercise to practice variables and data types

---
+++ day_1_variables_and_types.py
@@ -0,0 +1,10 @@
+name = "Mubashar"
+age = 31
+
+current_year = 2025
+calculate_upto_year=2035
+
+future_age=age+(calculate_upto_year-current_year)
+
+
+print(f"Hello {name}, you will be {future_age} years old in {calculate_upto_year}.")
