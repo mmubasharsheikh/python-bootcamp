10a18b765d17eee9dc574adb8fe31a16a8b38284
Mubashar Jamshad <m.mubasharsheikh@gmail.com>
2025-06-19 05:01:54 +0500

    Update in variables and types excercise to get values from user on runtime using method input(), also, introduced type casting

--- day_1_variables_and_types.py
+++ day_1_variables_and_types.py
@@ -1,10 +1,15 @@
-name = "Mubashar"
-age = 31
+## Problem: Get user's name his age and show him his age in 2035 in following format "Hi xyz, you will be x years old in 2035"
 
+# Getting user's data...
+name = input("Please enter your name: ")
+age = int(input("Please enter your age: "))
+
+# Setting curent and future age...
 current_year = 2025
-calculate_upto_year=2035
+calculate_upto_year = 2035
 
-future_age=age+(calculate_upto_year-current_year)
+# Logic to calculate future age...
+future_age = age + (calculate_upto_year-current_year)
 
-
+# Print final statement...
 print(f"Hello {name}, you will be {future_age} years old in {calculate_upto_year}.")
